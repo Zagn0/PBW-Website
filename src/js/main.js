8995ad47d23b35e270f1ba3f07ec8750fd29443c
Unknown <zagnoli.andrea@icloud.com>
2018-04-17 23:35:10 +0200

    first push

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('#fullPageWrapper').fullpage({
+		sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE'],
+		navigation: true,
+		navigationPosition: 'right',
+	});
+});
